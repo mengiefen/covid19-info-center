7caca0e7f4dad601b9df1469640770fc53284eca
mengiefen <mengiefen@gmail.com>
2022-04-13 11:53:38 +0300

    remove unncessary lines of code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { ThemeProvider } from 'styled-components';
@@ -20,32 +20,6 @@ const theme = {
     semiCyan: '#376e6faa',
     dark: '#1c3334',
     semiDark: '#1c333444',
-  },
-
-  colors1: {
-    lightPrimary: '#a86add',
-    primary: '#8549a7',
-    blackTwo: '#121212',
-    textMediumColor: '#555',
-    textLightColor: '#777',
-    backgroundColor: '#fff',
-    grey: '#e8e8e8',
-    shadowColor: '#c8c8c8',
-  },
-
-  colors2: {
-    bubbleGumPink: '#ff5ab7',
-    blackTwo: '#1e1e1e',
-    black1: '#000',
-    pinkishGrey: '#c4c4c4',
-    blackOne: '#121212',
-    greyBorder: ' #e8e8e8,',
-    coolBlue: '#4386bf',
-    pinkishGrey2: '#c7c7c7',
-    paleGrey: '#f5f6fa',
-    dirtyWhite: ' #fafafa',
-    warmGrey: '#888',
-    azure: ' #0290ff',
   },
 
   devices: {

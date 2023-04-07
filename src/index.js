c60a2341ce48fb0392e2985b6f74396d30b65ce5
mengiefen <mengiefen@gmail.com>
2023-04-07 13:09:11 +0300

    remove homepage from setting

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react';
-import { ThemeProvider } from 'styled-components';
-import './index.css';
-import App from './App';
-import { store, persistor } from './redux/configureStore';
-import NavBar from './components/NavBar/NavBar';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import { ThemeProvider } from "styled-components";
+import "./index.css";
+import App from "./App";
+import { store, persistor } from "./redux/configureStore";
+import NavBar from "./components/NavBar/NavBar";
 
 const theme = {
   colors: {
-    bg: '#2f4454',
-    shadow: '#c8c8c8',
-    text: '#fefefe',
-    coffee: '#2e151b',
-    pink: '#da7b93',
-    cyan: '#376e6f',
-    semiCyan: '#376e6faa',
-    dark: '#1c3334',
-    semiDark: '#1c333444',
+    bg: "#2f4454",
+    shadow: "#c8c8c8",
+    text: "#fefefe",
+    coffee: "#2e151b",
+    pink: "#da7b93",
+    cyan: "#376e6f",
+    semiCyan: "#376e6faa",
+    dark: "#1c3334",
+    semiDark: "#1c333444",
   },
 
   devices: {
-    mobile: '567px',
-    note: '767px',
-    tab: '1024px',
-    desktop: '1280px',
+    mobile: "567px",
+    note: "767px",
+    tab: "1024px",
+    desktop: "1280px",
   },
 };
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
@@ -45,5 +45,5 @@ root.render(
         </ThemeProvider>
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
